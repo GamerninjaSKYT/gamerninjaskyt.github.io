f376854dad6f52396db169e3f8daa754f52c0ce5
GamerninjaSKYT <gamerninjacz@gmail.com>
2023-08-08 20:08:01 +0200

    e
    e

---
+++ main.js
@@ -0,0 +1,6 @@
+function mainFunction() {
+    var main_i = document.getElementById("main_i").value;
+    var mainoutput;
+    mainoutput = main_i;
+    document.getElementById("maintxt").innerHTML = mainoutput;
+}
